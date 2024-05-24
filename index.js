04fadd3496c0bb022e19dfe7cfac9e73bd1b1726
milon-hossain <abireshan32@gmial.com>
2024-05-24 21:52:05 +0600

    create admin api and chcek it

--- index.js
+++ index.js
@@ -2,11 +2,27 @@ const express=require('express')
 const app=express()
 const cors=require('cors')
 const port=process.env.PORT||5000
+const jwt = require('jsonwebtoken');
 require('dotenv').config()
 //using middleware
 
 app.use(cors())
 app.use(express.json())
+const verifyToken=(req,res,next)=>{
+  console.log("inside verify token",req.headers.authorization);
+  if(!req.headers.authorization){
+    return res.status(401).send('unAuthorized access')
+  }
+  const token=req.headers.authorization
+  jwt.verify(token,process.env.ACCESS_TOKEN,function(err,decoded){
+    if(err){
+      return res.status(403).send('forbidden access')
+    }
+    req.decoded=decoded
+    next()
+  })
+  
+}
 
 app.get('/',async(req,res)=>{
     res.send('bistro boss in running')
@@ -55,6 +71,7 @@ async function run() {
     })
 //user releted api
     app.post('/users',async(req,res)=>{
+      
       const user=req.body
       //insert email if does't email
       const query={email:user.email}
@@ -66,7 +83,7 @@ async function run() {
       res.send(result)
     })
 
-    app.get('/users',async(req,res)=>{
+    app.get('/users',verifyToken,async(req,res)=>{
       const result=await userCollection.find().toArray()
       res.send(result)
     })
@@ -89,6 +106,31 @@ async function run() {
       }
       const result=await userCollection.updateOne(query,updatedDoc)
       res.send(result)
+    })
+
+    // jwt api
+
+
+    app.post('/jwt',async(req,res)=>{
+      const user=req.body
+     const token= jwt.sign(user,process.env.ACCESS_TOKEN,{expiresIn:'45day'})
+     res.send({token})
+    })
+
+    //check admin
+    app.get('/users/admin/:email',verifyToken,async(req,res)=>{
+        const email=req.params.email
+        if(email!==req.decoded.email){
+          return res.status(403).send('unAuthorized access')
+        }
+        const query={email:email}
+        const user=await userCollection.findOne(query)
+        let admin=false
+        if(user){
+          admin=user?.role==="admin"
+        }
+        res.send({admin})
+
     })
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
